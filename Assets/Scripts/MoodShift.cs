6da5e360f836da9143b935e63d932470838a21ec
Maf'j Alvarez <jay@rootinteractive.com>
2018-06-26 17:25:29 +0100

    Created start of MoodShift script for gradient lighting effects

---
+++ Assets/Scripts/MoodShift.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoodShift : MonoBehaviour {
+
+
+// MoodShift allows you to change the inside and outside lighting. 
+// Works with FirewatchFog script, Lighting Fog and a point light on a timeline
+// Define a mood type set the colour values and set with a gradient png file
+
+public script FirewatchFog;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
