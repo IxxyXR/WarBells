5f7b975e549c17f82fff6017fb8ee90a65320e3b
andybak <andy@andybak.net>
2018-06-27 12:51:02 +0100

    Better comments and test scene

--- Assets/MoodChange/MoodChangeMixerBehaviour.cs
+++ Assets/MoodChange/MoodChangeMixerBehaviour.cs
@@ -35,9 +35,9 @@ public class MoodChangeMixerBehaviour : PlayableBehaviour
             ScriptPlayable<MoodChangeBehaviour> inputPlayable = (ScriptPlayable<MoodChangeBehaviour>)playable.GetInput(i);
             MoodChangeBehaviour input = inputPlayable.GetBehaviour();
             
-            if (inputWeight > 0)
+            if (inputWeight > 0) // Assumes we'll only have 1 or 2 inputs with a weight above 0
             {
-                if (fogRamp1 == null)
+                if (fogRamp1 == null)  // Set start and end values to be the same
                 {
                     fogRamp1 = input.GradientFog;
                     fogRamp2 = input.GradientFog;
@@ -46,7 +46,7 @@ public class MoodChangeMixerBehaviour : PlayableBehaviour
                     LightColor1 = input.LightColor;
                     LightColor2 = input.LightColor;
                 }
-                else
+                else  // Set the final value and the blend amount
                 {
                     fogRamp2 = input.GradientFog;
                     DistanceFogColor2 = input.DistanceFogColor;
