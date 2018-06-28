22829193c2b0e2149c31af8369dc71d6accc361f
Andy Baker <andy@andybak.net>
2018-06-28 16:57:37 +0100

    Simple scene switcher (WIP)

---
+++ Assets/Scripts/Interactions/KeyboardInput.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeyboardInput : MonoBehaviour
+{
+
+	public int sceneNumber;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+	        SceneManager.LoadScene(sceneNumber);
+	        sceneNumber++;
+	        sceneNumber %= SceneManager.sceneCount;
+        }
+    }
+}
