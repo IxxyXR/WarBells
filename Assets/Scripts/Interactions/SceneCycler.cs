50e1c1a24bf186797edf5f232b82193a533a9bc0
Andy Baker <andy@andybak.net>
2018-06-29 13:12:58 +0100

    Scene cycler and some git noise

---
+++ Assets/Scripts/Interactions/SceneCycler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCycler : MonoBehaviour
+{
+	void Update ()
+	{
+        if (Input.GetKeyDown("space"))
+        {
+	        int nextSceneIndex = 0;
+	        var scene = SceneManager.GetActiveScene();
+	        var scenes = SceneManager.GetAllScenes();
+	        for (int i = 0; i < scenes.Length; i++)
+	        {
+		        if (scenes[i].name == scene.name)
+		        {
+			        nextSceneIndex = (i + 1) % scenes.Length;
+		        }
+	        }
+	        SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+}
