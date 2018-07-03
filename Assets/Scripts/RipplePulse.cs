0fce79b6445be03297b0f4ad16cdb72569df6983
Andy Baker <andy@andybak.net>
2018-07-03 19:05:39 +0100

    Add audio to bell ringing animation

--- Assets/Scripts/RipplePulse.cs
+++ Assets/Scripts/RipplePulse.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 public class RipplePulse : MonoBehaviour
 {
 	private ParticleSystem ps1, ps2;
-	private ParticleSystem.EmissionModule emission;
 	private SimpleAnimation bellAnimator;
-	private Vector3 shakeRot;
+	private AudioSource audioSource;
 
 	void Start()
 	{
 		ps1 = GetComponent<ParticleSystem>();
-		ps2 = GetComponentInChildren<ParticleSystem>();
-		emission = ps1.emission;
+		ps2 = gameObject.transform.GetChild(0).GetComponent<ParticleSystem>();
 		bellAnimator = gameObject.transform.parent.GetComponent<SimpleAnimation>();
+		audioSource = gameObject.transform.parent.GetComponent<AudioSource>();
+		bellAnimator.Stop();
 	}
 
 	public void SinglePulse()
 	{
 		ps1.Emit(1);
 		ps2.Emit(1);
+		bellAnimator.Rewind();
 		bellAnimator.Play();
+		audioSource.Play();
 	}
 }
+ 
