43f2c35bad96e9e9a4d20d15a051d179676fc614
Andy Baker <andy@andybak.net>
2018-07-03 11:40:28 +0100

    Simple timeline track for ripple effect

---
+++ Assets/Scripts/RipplePulse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RipplePulse : MonoBehaviour
+{
+	private ParticleSystem ps;
+	private ParticleSystem.EmissionModule emission;
+
+	void Start()
+	{
+		ps = GetComponent<ParticleSystem>();
+		emission = ps.emission;
+	}
+
+	public void SinglePulse() {
+		ps.Emit(1);	
+	}
+}
