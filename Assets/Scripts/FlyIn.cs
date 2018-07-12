923c84534266a79d98cbf091d3bac13aaf998bb8
Andy Baker <andy@andybak.net>
2018-07-12 13:05:12 +0100

    Document flyin script (WIP). Timeline tweak

---
+++ Assets/Scripts/FlyIn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyIn : MonoBehaviour {
+    
+	private Vector3 originalPosition;
+	private Quaternion originalRotation;
+
+	void Start()
+	{
+//		originalPosition = gameObject.transform.position;
+//		originalRotation = gameObject.transform.rotation;
+//		gameObject.transform.position = originalPosition - new Vector3(0, 0, 10);
+//		int id = LeanTween.move(gameObject, originalPosition, 1f).id;
+//		LTDescr d = LeanTween.descr(id);
+//		d?.setEase(LeanTweenType.easeInOutBack);
+	}
+	
+}
