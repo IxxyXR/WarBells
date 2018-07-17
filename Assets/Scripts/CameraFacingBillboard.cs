b1c8849db50c4821d81e738e9a377d2d534bac6a
Andy Baker <andy@andybak.net>
2018-07-17 13:55:57 +0100

    Tree LOD

--- Assets/Scripts/CameraFacingBillboard.cs
+++ Assets/Scripts/CameraFacingBillboard.cs
@@ -13,8 +13,10 @@ public class CameraFacingBillboard : MonoBehaviour
 
 	void Update ()
 	{
+		var rot = camera.transform.rotation;
+		rot.x = 0;
 		transform.LookAt(
-			transform.position + camera.transform.rotation * Vector3.forward,
+			transform.position + rot * Vector3.forward,
 			Vector3.up
 		);
 	}
