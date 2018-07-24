7b1067e00143290969bc032da7d3301dfe34eb11
Andy Baker <andy@andybak.net>
2018-07-24 10:48:57 +0100

    Script to enable bell smashing

---
+++ Assets/EnableSmashing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Interactions;
+using UnityEngine;
+
+public class EnableSmashing : MonoBehaviour {
+
+	void Start () {
+		for (int i = 0; i < gameObject.transform.childCount; i++)
+		{	
+			var g = gameObject.transform.GetChild(i).gameObject;
+			g.GetComponent<Selectable>().enabled = false;
+			g.GetComponent<Smashable>().enabled = false;
+		}
+	}
+	
+	public void DoEnableSmashing () {
+		for (int i = 0; i < gameObject.transform.childCount; i++)
+		{	
+			var g = gameObject.transform.GetChild(i).gameObject;
+			g.GetComponent<Selectable>().enabled = true;
+			g.GetComponent<Smashable>().enabled = true;
+		}
+	}
+}
