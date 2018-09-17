2fec778f0248fe863a89464c40a013cc56d8eedd
Andy Baker <andy@andybak.net>
2018-09-17 12:42:14 +0100

    Import dominika's bell with physics

---
+++ Assets/Scripts/PlaySoundOnCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnCollision : MonoBehaviour{
+
+    AudioSource myaudio;
+
+    void Start() {
+
+        myaudio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other){
+
+        if (other.gameObject.tag == "playsound" ){
+            myaudio.Play();
+            Debug.Log("How do you enjoy the sound?");
+        }
+    }
+}
